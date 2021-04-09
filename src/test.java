2a74c013f3655d518a78e5a307aaa2b1d35edffc
dongdd159 <79952800+dongdd159@users.noreply.github.com>
2021-04-09 16:07:22 +0700

    2dva3d

---
+++ src/test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        Point2D point2d= new Point2D(2,3);
+        System.out.println(point2d.toString());
+        Point3D point3d= new Point3D(2,3,4);
+        System.out.println(point3d.toString());
+    }
+}
